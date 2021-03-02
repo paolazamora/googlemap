98a0b827804527485e407d1baf5363a3dd9bb947
paolazamora <pz270716@gmail.com>
2021-03-02 16:03:11 -0600

    commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+import {Helmet} from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+      <Helmet>
+      <script defer data-key="23aa3957-58d8-41f4-af6c-ad4635e25ce0" src="https://widget.tochat.be/bundle.js"></script> 
+       </Helmet>
+      
+    </div>
+  );
+}
+
+export default App;
